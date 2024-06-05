2bad3f1bde24a5333ac27445f8b391e548ba046a
bmahendra16 <154310152+bmahendra16@users.noreply.github.com>
2024-06-05 15:27:17 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 123
+let accountEmail ='mbohara991@gmail.com'
+var accountPassword ='123456'
+accountCity ='Kathmandu'
+let accountState;
+
+// accountId = 111 // const cannnot allowed to re-declear and re-assign 
+
+
+ accountEmail ="nishabist17.com"
+ accountPassword ="987654"
+ accountCity ="Mahendranagar"
+
+ console.log(accountId);
+
+ /*
+    prefer not to use var 
+    because of issue in block scope and functional scope
+ */
+
+ console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
