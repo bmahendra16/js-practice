8fa20f827df59bc2c9f826aed9798a0ca56b215d
bmahendra16 <154310152+bmahendra16@users.noreply.github.com>
2024-06-06 02:05:47 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict";  //treat all version as newer version
+
+//alert( 3 + 3) // we are using nodejs not browser
+
+console.log( 3 + 3); // code readabilty should be high
+
+
+console.log("Mahendra");
+
+
+let name ="Mahendra"
+let age = 23
+let isLoggedIn = true
+let state;
+
+//number range => 2 to power 53
+// bigInt is used for bigger number
+// srting => ""
+//boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+//object
+
+console.log(typeof "Mahendra"); //string
+console.log(typeof age); //number
+console.log(typeof true);//boolean
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
