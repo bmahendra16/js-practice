833cdba9fcd2911d70482908b237c997e9507dbc
bmahendra16 <154310152+bmahendra16@users.noreply.github.com>
2024-06-08 15:36:46 +0000

    comparison between different datatypes

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1); this only compare the number 
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+
+console.log("2" === 1); // this compare number and datatypes
